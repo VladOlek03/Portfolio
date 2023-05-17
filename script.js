49a8300593694d540c6cfe2f1efd9004310a2848
Vlad O <vladolek2003@gmail.com>
2023-05-16 20:12:48 -0700

    Added a script which changes the color and size of each individual letter in the h1 element when the mouse moves over it, and changes it back once the mouse moves over it again. There is another script which also relies on the IntersectionObserver to show the .control1 class elements whenever the user scrolls down the page. The h1 element inside the control box also no longer functions along with the mouse x and y script.

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ document.addEventListener("mousemove", parallax);
 
 function parallax(e) {
     document.querySelectorAll(".object").forEach(function(move){
-        let x = (e.clientX + 20) / 300;
-        let y = (e.clientY + 20) / 300;
+        let x = (e.clientX + 20) / -50;
+        let y = (e.clientY + 20) / -50;
 
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
     })
 
-    document.querySelectorAll(".object2").forEach(function(move){
-        let x = (e.clientX + 20) / -300;
-        let y = (e.clientY + 20) / -300;
-
-        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
-    })
+    // document.querySelectorAll(".object2").forEach(function(move){
+    //     let x = (e.clientX + 20) / -50;
+    //     let y = (e.clientY + 20) / -50;
+    //
+    //     move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    // })
 
     document.querySelectorAll(".object3").forEach(function(move){
-        let x = (e.clientX + 20) / 300;
-        let y = (e.clientY + 20) / 300;
+        let x = (e.clientX + 20) / 50;
+        let y = (e.clientY + 20) / 50;
 
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
     })
@@ -38,9 +38,31 @@ document.addEventListener('DOMContentLoaded', function() {
         span.addEventListener('mouseover', function() {
             if (this.style.color == '') {
                 this.style.color = 'white';
+                this.style.fontSize = '450px';
+                this.style.transitionTimingFunction = 'ease';
+                this.style.transition = '2s';
             } else {
                 this.style.color = '';
+                this.style.fontSize = '400px';
             }
         });
     });
 });
+
+let observe = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } //else {
+        //     entry.target.classList.remove('show');
+        // }
+    })
+})
+
+let boxElements = document.querySelectorAll('.text');
+boxElements.forEach((element) => observe.observe(element));
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
