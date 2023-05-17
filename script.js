3fc37a748db05e761a2a6db8e48ae0f61af4110f
Vlad O <vladolek2003@gmail.com>
2023-05-16 18:01:54 -0700

    Added a separate offset script for each of the object h1 and h2 elements within the HTML, and added a mouse-over effect to the h1 element.

--- script.js
+++ script.js
@@ -2,10 +2,45 @@ document.addEventListener("mousemove", parallax);
 
 function parallax(e) {
     document.querySelectorAll(".object").forEach(function(move){
-        let moving = move.getAttribute("data-value");
-        let x = (e.clientX + 20) / 100;
-        let y = (e.clientY + 20) / 100;
+        let x = (e.clientX + 20) / 300;
+        let y = (e.clientY + 20) / 300;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    })
+
+    document.querySelectorAll(".object2").forEach(function(move){
+        let x = (e.clientX + 20) / -300;
+        let y = (e.clientY + 20) / -300;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    })
+
+    document.querySelectorAll(".object3").forEach(function(move){
+        let x = (e.clientX + 20) / 300;
+        let y = (e.clientY + 20) / 300;
 
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
     })
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const object2 = document.querySelector('.object2');
+
+    const letters = object2.textContent.trim().split('');
+
+    object2.textContent = ''; // Clear the original text content
+
+    letters.forEach(function(letter) {
+        const span = document.createElement('span');
+        span.textContent = letter;
+        object2.appendChild(span);
+
+        span.addEventListener('mouseover', function() {
+            if (this.style.color == '') {
+                this.style.color = 'white';
+            } else {
+                this.style.color = '';
+            }
+        });
+    });
+});
