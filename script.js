8ece77c92d1f6534429095467c7a7c70c7c95340
Vlad O <vladolek2003@gmail.com>
2023-05-23 12:39:26 -0700

    Added an observe function to the portfolio image. Added an event listener to the portfolio image that checks for mouse over event, and shows the social information, while making the image slightly bigger. The opposite of this function was also added. Two functions named hideInformation and showInformation were added to hide the main information that is first present alongside the image.

--- script.js
+++ script.js
@@ -55,15 +55,52 @@ let observe = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } //else {
+            entry.target.style.opacity = '1';
+            entry.target.style.marginTop = '0px';
+            entry.target.style.filter = 'blur(0)';
+         } //else {
         //     entry.target.classList.remove('show');
         // }
     })
 })
 
 let boxElements = document.querySelectorAll('.text');
+let portfolioImages = document.querySelectorAll('.portfolio-image');
+
 boxElements.forEach((element) => observe.observe(element));
+portfolioImages.forEach((element) => observe.observe(element));
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
+}
+
+let portfolioImage1 = document.querySelector('.information img');
+let socialInfo = document.querySelector('.social-info');
+let informationBox = document.querySelector('.control1:first-of-type');
+
+portfolioImage1.addEventListener('mouseover', function() {
+    hideInformation();
+    socialInfo.style.transform = 'translateX(80%)';
+    socialInfo.style.filter = 'blur(0)';
+    portfolioImage1.style.width = '520px';
+    portfolioImage1.style.transition = 'all 1s';
+})
+
+informationBox.addEventListener('mouseleave', function() {
+    showInformation();
+    socialInfo.style.transform = 'translateX(-1000%)';
+    portfolioImage1.style.width = '500px';
+    portfolioImage1.style.transition = 'all 1s';
+})
+
+let infoText = document.querySelector('.information h2');
+
+function hideInformation() {
+    infoText.style.transition = 'all 1s';
+    infoText.style.opacity = '0';
+}
+
+function showInformation() {
+    infoText.style.transition = 'all 4s';
+    infoText.style.opacity = '1';
 }
